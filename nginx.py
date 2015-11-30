a5dcc25d1431118e7ef1805422837ac2b3e7fba3
Manuel Villaverde <vialmajo@gmail.com>
2015-11-30 17:03:09 +0100

    Cambiamos los paths de nginx para poner los que suelen tener las idstros

--- nginx.py
+++ nginx.py
@@ -5,18 +5,18 @@
 #Script para añadir archivo de configuración ngix para aplicaciones python o php
 #
 import os
-pathConfNginx="nginxconf/"
+pathConfNginx=os.path.dirname('/etc/nginx/') #Path a la configuracion de nginx
 
 #Funcionar principal que se encarg de la configuracion de ngix
 def nginx_main(domain,project,project_type):
 
-    dir = os.path.dirname(pathConfNginx)
     try:
-        os.stat(dir)
+        os.stat(pathConfNginx)
     except:
         print "Error el direcotrio %s no exixta\nNo se seguira creando nada" % (pathConfNginx)
         return
     nginx_conf(domain, project, project_type)
+    nginx_ln(project)
 
 #funcion que genera archivo nginx en función de los parámetros indicados
 #parametros: domain = dominio del proyecto; project = nombre del proyecto
@@ -27,13 +27,14 @@ def nginx_conf(domain, project, project_type):
         fFile = open("templates/nginx/nginx_python_template.conf", "r")
     elif project_type == 'php':
         fFile = open("templates/nginx/nginx_php_template.conf", "r")
-    fFile_new = open(pathConfNginx+project+".conf", "w")
+    fFile_new = open(pathConfNginx+"/sites-available/"+project+".conf", "w")
     content = fFile.read()
     content = content.replace('{project}', project)
     content = content.replace('{domain}', domain)
     fFile_new.write(content)
-    print "Creada la configuracion de nginx"
+    print("Creada la configuracion de nginx")
 
 def nginx_ln(project):
-    comando = 'ln -s '+pathConfNginx+project+' '+pathConfNginx+'enlace'
+    comando = 'ln -s '+pathConfNginx+'/sites-available/'+project+' '+pathConfNginx+'sites-enabled'+project
     os.system(comando)
+    print("Creado en enlace simbolico de nginx para activar el virtualhost")
